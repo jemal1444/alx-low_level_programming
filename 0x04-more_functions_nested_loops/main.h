06f64f60f1269d26c93603e36768ffb803764747
jemal <jemalmit@gmail.com>
2023-03-23 08:20:47 -0700

    more function nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int  _isupper();
+int _isdigit();
+void print_numbers();
+int mul();
+int _putchar(char c);
+void print_most_numbers ();
+void more_numbers();
+void print_diagonal();
+void print_line();
+void print_square ();
+void print_triangle ();
+void print_number();
+#endif
