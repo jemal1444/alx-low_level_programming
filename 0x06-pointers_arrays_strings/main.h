9d8bfc1f5be2eadea7c9c2637fc49e184d791cb7
jemal <jemalmit@gmail.com>
2023-03-29 04:02:48 -0700

    more pointer array project

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int _putchar(char c);
+void print_number(int n);
+char *rot13(char *s);
+char *leet(char *s);
+char *cap_string(char *s);
+char *string_toupper(char *p);
+void reverse_array(int *a, int n);
