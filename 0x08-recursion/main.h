eb2745a5a5978542ef2cb6dce1e4be83e05fecdf
jemal <jemalmit@gmail.com>
2023-04-05 02:25:37 -0700

    Recursion ALX project

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
+int is_prime_number(int n);
+int check_prime(int n, int i);
+int _strlen_recursion(char *s);
+int check_palindrome(char *s);
+int is_palindrome(char *s);
+int pali(char *s, char *e);
+int wildcmp(char *s1, char *s2);
+#endif
+
