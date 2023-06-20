63c41160652ae94c39206682a6e4bff20c65bd38
jemal1444 <jemal.eyasu@yahoo.com>
2023-06-19 23:42:07 -0700

    This 0x18-dynamic_libraries project is for ALX batch of 2023

---
+++ 0x18-dynamic_libraries/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers.
+ * @a: first input value.
+ * @b: second input value.
+ * Return: the result of sum.
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
---
+++ 0x18-dynamic_libraries/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - add two integers.
+ * @a: first input value.
+ * @b: second input value.
+ * Return: the result of sum.
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
